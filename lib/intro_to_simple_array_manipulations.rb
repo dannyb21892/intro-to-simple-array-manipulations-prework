8fd17d55a1bac5f6efdb5066b82c0d7c30f5d065
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-07 22:43:52 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,19 @@ end
 def using_insert(array, element)
   array.insert(4, element)
 end
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+end
