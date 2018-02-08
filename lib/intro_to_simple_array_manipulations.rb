a11acd486b450d879fe103d4ac5344af35d0bd96
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-08 00:16:58 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,9 +39,9 @@ def using_flatten(array)
 end
 
 def using_delete(array, element)
-  
+  array.delete(element)
 end
 
-def using_delete_at(array, element)
-  
+def using_delete_at(array, index)
+  array.delete_at(index)
 end
